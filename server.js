2c6dac94a68ddcbbdc276379ec66f190c90f8531
Mohalidin Lidasan <mohalidinlidasan333@gmail.com>
2024-02-25 18:19:05 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get("/greet", (req, res) => {
+    return res.send("hello world!");
+});
+
+app.listen(port, () => console.log("Server running on port:", port));
